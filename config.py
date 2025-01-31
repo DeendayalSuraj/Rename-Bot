617de745cd045b56e8b8dd9db9c0053203447351
DeendayalSuraj <surajkumar62067542@gmail.com>
2025-01-31 16:37:00 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,23 +3,23 @@ import os
 
 
 # Required Variables Config
-API_ID = int(os.environ.get("API_ID", ""))
-API_HASH = os.environ.get("API_HASH", "")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-ADMIN = int(os.environ.get("ADMIN", ""))
+API_ID = int(os.environ.get("API_ID", "26992030"))
+API_HASH = os.environ.get("API_HASH", "4da7d71c6bc4512a886e41aca83a5ee3")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "8026778102:AAEsTeZXg7kAstTC2jtmqSchmLRcZD-Vyds")
+ADMIN = int(os.environ.get("ADMIN", "6476706511"))
 
 
 # Premium 4GB Renaming Client Config
-STRING_SESSION = os.environ.get("STRING_SESSION", "")
+STRING_SESSION = os.environ.get("STRING_SESSION", "BQGb3Z4AvMTlyWgkLEzuKuobNMeGmmeMmyg2XQn0W35kxB3YMD0AebF1ihEtTarmYOSNNPB_Q7yW2kn4esXIbn7pAzGzm5f7_r7Ej3RRvEDFMoYxHGQI36HHYbLzR8MAN3LBMRHgCryPu9yqSNuHqHmnEHN6s0n6nTEBZKAUbnq-Qulod9P-S4e88i_5nLIPNoBZ9nvnQHWhY_-Kbw1T6zDOKkOfkNV4rk4NH2NHJYBAOJpyOHYwPqx23B4XCwJ4mPefq53HTEM6AhXJFpWzSQFi_0pWX-tYeKDKwbMgol8ocA7xQod9EixOOrs06V6hWwKD6LE7FwYUH2izHKH9BoeN6xuISAAAAAGCCrLPAA")
 
 
 # Log & Force Channel Config
-FORCE_SUBS = os.environ.get("FORCE_SUBS", "")
-LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", ""))
+FORCE_SUBS = os.environ.get("FORCE_SUBS", "-1002448396142")
+LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002280223922"))
 
 
 # Mongo DB Database Config
-DATABASE_URL = os.environ.get("DATABASE_URL", "")
+DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://sunil664023: eieqy2jRVIbzD29r@cluster0.ezot5.mongodb .net/?retryWrites=true&w=majority&appName= Clustero")
 DATABASE_NAME = os.environ.get("DATABASE_NAME", "TechifyBots")
 
 
